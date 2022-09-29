e7903234cefeb4abbe2a7ffc570411b5e61c8cf1
IvanAAguiar <ivan.araujo.aguiar@gmail.com>
2022-09-29 21:06:40 +0100

    Initial Commit

---
+++ ClimateArticlesOf2021FromNOAA_App/ClimateArticlesOf2021FromNOAA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateArticlesOf2021FromNOAA_AppApp.swift
+//  ClimateArticlesOf2021FromNOAA_App
+//
+//  Created by Ivan Aguiar on 29/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClimateArticlesOf2021FromNOAA_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimateArticlesOf2021FromNOAA_App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClimateArticlesOf2021FromNOAA_App
+//
+//  Created by Ivan Aguiar on 29/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
