7bb6797920313fe7c5895e83002728e994d5b2f1
IvanAAguiar <ivan.araujo.aguiar@gmail.com>
2022-10-03 13:31:43 +0100

    create toogle feature at WeSplit Project

--- ClimateArticlesOf2021FromNOAA_App/WeSplit/WeSplitProjectView.swift
+++ ClimateArticlesOf2021FromNOAA_App/WeSplit/WeSplitProjectView.swift
@@ -9,60 +9,87 @@ import Foundation
 import SwiftUI
 
 struct WeSplitProjectView: View {
-    
-//    @FocusState private var focusedInput: Field?
-    
+        
     @State private var billAmount = 0.0
     @State private var numberOfPeople = 1
     @State private var tipPercentage = 0
+    @State private var ivaPercentage = 23
+    @State private var includeIVA = false
     
     @FocusState private var billAmountIsFocused: Bool
     
     let tipPercentages = [0, 5, 10, 15, 20, 25]
+    let ivaPercentages = [23, 13, 6]
+    
+    var iva: Double {
+        let ivaSelection = Double(ivaPercentage)
+        
+        let ivaAmount = billAmount / 100 * ivaSelection
+                
+        return ivaAmount
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-
+        
         let tipValue = billAmount / 100 * tipSelection
-        let grandTotal = billAmount + tipValue
+        let grandTotal: Double
+        
+        if includeIVA {
+            grandTotal = billAmount + tipValue + iva
+        } else {
+            grandTotal = billAmount + tipValue
+        }
+        
         let amountPerPerson = grandTotal / peopleCount
-
+        
         return amountPerPerson
     }
     
     var body: some View {
-        VStack {
-            Form {
-                Section {
-                    TextField("Enter the amount", value: $billAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
-                        .keyboardType(.decimalPad)
-                        .focused($billAmountIsFocused)
-                    Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) people")
+        NavigationView {
+            VStack {
+                Form {
+                    Section (header: Text("Amount to split")) {
+                        TextField("Enter the amount", value: $billAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                            .keyboardType(.decimalPad)
+                            .focused($billAmountIsFocused)
+                        Picker("Number of People", selection: $numberOfPeople) {
+                            ForEach(2..<100) {
+                                Text("\($0) people")
+                            }
                         }
                     }
-                }  header: {
-                    Text("WeSplit")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundColor(.black)
-                        .textCase(.none)
-                }
-                Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
-                            
+                    
+                    Section (header: Text("How much tip do you want to leave?")) {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
                         }
                         .pickerStyle(.segmented)
                     }
-                } header: {
-               Text("How much tip do you want to leave?")
-           }
-                Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                
+                    Section (header: Text("IVA")) {
+                        Toggle("To include IVA taxes", isOn: $includeIVA)
+                            .toggleStyle(.switch)
+                                                
+                         if includeIVA {
+                            Picker("IVA taxes", selection: $ivaPercentage) {
+                                ForEach(ivaPercentages, id: \.self) { value in
+                                    Text("\(value)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                            Text(iva, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                            Link("About", destination: URL(string: "https://www.e-konomista.pt/como-calcular-o-iva")!)
+                         }
+                    }
+                    
+                    Section (header: Text("total per person")){
+                        Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                    }
                 }
             }
             .formStyle(.automatic)
@@ -74,8 +101,9 @@ struct WeSplitProjectView: View {
                     }
                 }
             }
+            .navigationTitle("WeSplit")
+            .tint(.blue)
         }
-        .background(.mint.opacity(0.4))
     }
 }
 
