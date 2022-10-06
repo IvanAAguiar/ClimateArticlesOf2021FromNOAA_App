88b000403fe9a449ac472838d31d3e2aefdafc2a
IvanAAguiar <ivan.araujo.aguiar@gmail.com>
2022-10-06 20:56:33 +0100

    Refactoring BetterRest Project: applying MVVM. (address #6)

--- ProjectLibraryApp/BetterRest/BetterRest/BetterRestProjectView.swift
+++ ProjectLibraryApp/BetterRest/BetterRest/BetterRestProjectView.swift
@@ -10,21 +10,6 @@ import SwiftUI
 
 struct BetterRestProjectView: View {
     
-//    @State private var wakeUp = defaultWakeTime
-//    @State private var sleepAmount: Double = 8.0
-//    @State private var coffeeAmount: Int = 1
-//    
-//    @State private var alertTitle = ""
-//    @State private var alertMessage = ""
-//    @State private var showingAlert = false
-//
-//    static var defaultWakeTime: Date {
-//        var components = DateComponents()
-//        components.hour = 7
-//        components.minute = 0
-//        return Calendar.current.date(from: components) ?? Date.now
-//    }
-    
     @ObservedObject var vm: BetterRestMLViewModel = BetterRestMLViewModel()
     
     var body: some View {
@@ -60,27 +45,6 @@ struct BetterRestProjectView: View {
             }
         }
     }
-    
-//    func calculateBedTime() {
-//        do {
-//            let config = MLModelConfiguration()
-//            let model = try SleepCalculator(configuration: config)
-//
-//            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-//            let hour = (components.hour ?? 0) * 60 * 60
-//            let minute = (components.minute ?? 0) * 60
-//
-//            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-//
-//            let sleepTime = wakeUp - prediction.actualSleep
-//            alertTitle = "Your ideal bedtime is..."
-//            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-//        } catch {
-//                alertTitle = "Error"
-//                alertMessage = "Sorry, there was a problem calculating your badtime."
-//            }
-//        showingAlert = true
-//    }
 }
 
 struct BetterRestProject_Previews: PreviewProvider {
