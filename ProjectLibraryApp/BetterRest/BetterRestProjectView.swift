0afb2e67ac5f7ab6929de456773dec486aa8e6ec
IvanAAguiar <ivan.araujo.aguiar@gmail.com>
2022-10-06 19:09:22 +0100

    Create BetterRest Project (address #6)

--- ProjectLibraryApp/BetterRest/BetterRestProjectView.swift
+++ ProjectLibraryApp/BetterRest/BetterRestProjectView.swift
@@ -5,11 +5,79 @@
 //  Created by Ivan Aguiar on 06/10/2022.
 //
 
+import CoreML
 import SwiftUI
 
 struct BetterRestProjectView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount: Double = 8.0
+    @State private var coffeeAmount: Int = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Form {
+                    Section("When do you want to wake up?") {
+                        DatePicker("Hour and minutes", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    }
+                    Section("Desired amount of sleep") {
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    }
+                    Section("Daily coffee intake") {
+                        Picker("Daily coffee intake", selection: $coffeeAmount) {
+                            ForEach (0...25, id: \.self) { coffee in
+                                Text(coffee, format: .number)
+                            }
+                        }
+                    }
+                }
+                .font(.headline)
+                .navigationTitle("BetterRest")
+                
+                Button("Calculate", action: calculateBedTime)
+                    .ButtonCustomizedStyle()
+                    .alert(alertTitle, isPresented: $showingAlert) {
+                        Button("Ok") {
+                            
+                        }
+                    } message: {
+                        Text(alertMessage)
+                    }
+            }
+        }
+    }
+    
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+                alertTitle = "Error"
+                alertMessage = "Sorry, there was a problem calculating your badtime."
+            }
+        showingAlert = true
     }
 }
 
--- ProjectLibraryApp/BetterRest/FirstBetterRestView.swift
+++ ProjectLibraryApp/BetterRest/FirstBetterRestView.swift
@@ -20,6 +20,7 @@ struct FirstBetterRestView: View {
                 }
                 .tag(0)
             ExplanationView(title: "Better Rest", description: """
+            This project was inspired from HACKING WITH SWIFT. The principle goal is to understand how MLCreate works
             The reason we have a fairly simple project is because is to introduce one of the true power features of iOS development: machine learning (ML).
             All iPhones come with a technology called Core ML built right in, which allows us to write code that makes predictions about new data based on previous data it has seen. We’ll start with some raw data, give that to our Mac as training data, then use the results to build an app able to make accurate estimates about new data – all on device, and with complete privacy for users.
             The actual app we’re building is called BetterRest, and it’s designed to help coffee drinkers get a good night’s sleep by asking them three questions:
